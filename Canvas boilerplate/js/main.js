97a3ad6b27bd0833cd7c78c2144f4e3d30a0657f
Juan Manuel Careaga <jmcareaga@ciencias.unam.mx>
2019-06-21 21:37:22 -0500

    Added some comments

--- Canvas boilerplate/js/main.js
+++ Canvas boilerplate/js/main.js
@@ -1,14 +1,17 @@
+// Create the canvas and context
 const canvas = document.createElement('canvas');
 const ctx    = canvas.getContext('2d');
-
-canvas.style.backgroundColor = 'black';
 document.body.appendChild(canvas);
+
+// Setup for fullscreen canvas
+canvas.style.backgroundColor = 'black';
 (function init(){
     canvas.width = innerWidth;
     canvas.height = innerHeight;
     addEventListener('resize', init);
 })();
 
+// Main loop for animations
 (function animate(){
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, innerWidth, innerHeight);
