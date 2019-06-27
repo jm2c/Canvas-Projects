6a36c40592d4af0b4c9e1fa25a593eadd22038d9
Juan Manuel Careaga <jmcareaga@ciencias.unam.mx>
2019-06-26 22:28:31 -0500

    Added some utils

--- Canvas boilerplate/js/main.js
+++ Canvas boilerplate/js/main.js
@@ -11,9 +11,43 @@ canvas.style.backgroundColor = 'black';
     addEventListener('resize', init);
 })();
 
+// Get mouse coords
+let mouseX, mouseY;
+addEventListener('mousemove', evt => {
+    mouseX = evt.x;
+    mouseY = evt.y;
+});
+
+// Ready to use Object
+class Obj {
+    constructor (x, y, r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+    draw () {
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+    update () {
+        this.draw();
+    }
+}
+const objs = [];
+for (let i = 0; i < 1; i++) {
+    objs.push(
+        new Obj(canvas.width / 2, canvas.height / 2, 5)
+    );
+}
+
 // Main loop for animations
 (function animate(){
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     // Things to update
+    objs.forEach( obj => {
+        obj.update();
+    })
 })();
