49f9614b4e36b0ee006444c1a85066b89a1b8a30
Juan Manuel Careaga <jmcareaga@ciencias.unam.mx>
2019-06-26 22:34:10 -0500

    Changed to english

--- Follow the cursor/app.js
+++ Follow the cursor/app.js
@@ -1,8 +1,8 @@
-// Crear el lienzo de dibujo
+// Create the canvas
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 
-// Adaptarlo al tamaño de la ventana e incrustarlo dentro del documento HTML
+// Adjust the canvas size
 (function init(){
     canvas.width = innerWidth;
     canvas.height = innerHeight;
@@ -10,7 +10,7 @@ const ctx = canvas.getContext('2d');
 })();
 document.body.appendChild(canvas);
 
-// Obtener constantemente las coordenadas del ratón
+// Get mouse coords
 let mouseX, mouseY;
 addEventListener('mousemove', evt => {
     mouseX = evt.x;
@@ -18,9 +18,9 @@ addEventListener('mousemove', evt => {
 });
 
 /**
- * Crea un círculo del tamaño y color dados que sigue al ratón
- * @param r = (Int) radio de la partícula
- * @param color = (String) color de la partícula en cualquier formato aceptado
+ * Create a circle of the given size and color that follows the mouse
+ * @param r = (Int) particle radius
+ * @param color = (String) particle color
  */
 class Particle {
     constructor(r, color){
@@ -51,10 +51,10 @@ class Particle {
     }
 }
 
-// Crea una partícula
+// Add the particle
 p1 = new Particle(10, '#22a6b3');
 
-// Loop principal de la animación
+// Main animation loop
 (function animate(){
     requestAnimationFrame(animate);
     ctx.fillStyle = 'rgba(255,255,255,0.2)';
