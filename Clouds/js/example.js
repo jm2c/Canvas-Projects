8669fb5d9bb47ef2cda2a26059383ea6468ec509
Juan Manuel Careaga <jmcareaga@ciencias.unam.mx>
2019-07-01 15:31:35 -0500

    Example updated

--- Clouds/js/example.js
+++ Clouds/js/example.js
@@ -11,25 +11,19 @@ const ctx = canvas.getContext('2d');
 })();
 
 const clouds = [];
-function addCloud() {
+for (let i = 0; i < 8; i++) {
     const size = Math.random() * 20 + 5 | 0;
     const height = Math.random() * (canvas.height / 2) + 2 * size | 0;
     const vel = - size * 0.05;
     clouds.push(
         new Cloud(
-            canvas.width + 6 * size,
+            Math.random() * canvas.width + 100,
             height,
             size,
             vel
         )
     );
 }
-addCloud();
-
-setInterval(() => {
-    if (clouds.length < 12)
-        addCloud();
-}, 5E3);
 
 // Main loop for animations
 (function animate() {
@@ -38,11 +32,12 @@ setInterval(() => {
 
     // Clear invisible clouds
     for (let c = 0; c < clouds.length; c++) {
-        if (clouds[c].x < - 6 * clouds[c].size)
-            clouds.splice(c, 1);
+        if (clouds[c].x < - 6 * clouds[c].size) {
+            clouds[c].x = canvas.width + 6 * clouds[c].size;
+        }
     }
-
-    // Animate every single cloud
+    
+    // Animate every single cloud    
     clouds.forEach(cloud => {
         cloud.update(ctx);
     });
