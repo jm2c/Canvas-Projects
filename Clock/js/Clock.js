05c8ad434954c9845cbad390cfc354046adc65d0
Juan Manuel Careaga <jmcareaga@ciencias.unam.mx>
2019-07-11 09:42:19 -0500

    Changes in style. Minute handler smooth movment

--- Clock/js/Clock.js
+++ Clock/js/Clock.js
@@ -2,7 +2,13 @@ export default class Clock {
     constructor(twentyfour = false, milliseconds = false) {
         this.twentyfour = twentyfour;
         this.milliseconds = milliseconds;
-        this.update();
+        this.setSystemTime();
+
+        // Canvas Personilizer
+        this.name = 'QUARTZ';
+        this.fgColor = 'black';
+        this.bgColor = 'white';
+        this.handlersColor = 'midnightblue';
     }
 
     // Getters for system time
@@ -11,7 +17,7 @@ export default class Clock {
     getSystemSeconds()      { return new Date().getSeconds();      }
     getSystemMilliseconds() { return new Date().getMilliseconds(); }
 
-    update() {
+    setSystemTime() {
         this.h = this.getSystemHours();
         this.m = this.getSystemMinutes();
         this.s = this.getSystemSeconds();
@@ -55,13 +61,15 @@ export default class Clock {
         ctx.clearRect(-size/2,-size/2,size,size);
         ctx.beginPath();
         // Draw the circle
-        ctx.strokeStyle = 'black';
+        ctx.strokeStyle = this.fgColor;
+        ctx.fillStyle = this.bgColor;
         ctx.lineWidth = size / 40;
         ctx.arc(0, 0, size*0.95 / 2, 0, 2 * Math.PI);
+        ctx.fill();
         ctx.stroke();
 
         // Draw marks
-        ctx.fillStyle = 'black';
+        ctx.fillStyle = this.fgColor;
         dot(ctx, 0, 0, size / 50);
         dot(ctx,  alignmentFactor * cos60, -alignmentFactor * sin60, size / 80);
         dot(ctx,  alignmentFactor * cos30, -alignmentFactor * sin30, size / 80);
@@ -80,11 +88,13 @@ export default class Clock {
         ctx.fillText('3', alignmentFactor, 0);
         ctx.fillText('6', 0, alignmentFactor);
         ctx.fillText('9', -alignmentFactor, 0);
+        ctx.font = `${size/30}px serif`;
+        ctx.fillText(this.name, 0, alignmentFactor-size/8);
 
         // Draw handlers
         ctx.lineCap = 'round';
+        ctx.strokeStyle = this.handlersColor;
         // Hour handler
-        ctx.strokeStyle = 'steelblue';
         let r = -(size / 2)*.55;
         let angle = (this.h % 24)*30 + this.m * 0.5;
         let x = r*Math.cos((angle+90)*Math.PI/180);
@@ -95,9 +105,8 @@ export default class Clock {
         ctx.lineTo(x, y);
         ctx.stroke();
         // Minute handler
-        ctx.strokeStyle = 'royalblue';
         r = -(size / 2)*.65;
-        angle = this.m*6;
+        angle = this.m*6 + this.s * .1;
         x = r*Math.cos((angle+90)*Math.PI/180);
         y = r*Math.sin((angle+90)*Math.PI/180);
         ctx.beginPath();
@@ -106,7 +115,6 @@ export default class Clock {
         ctx.lineTo(x, y);
         ctx.stroke();
         // Seconds handler
-        ctx.strokeStyle = 'midnightblue';
         r = -(size / 2)*.75;
         angle = this.s*6;
         x = r*Math.cos((angle+90)*Math.PI/180);
@@ -116,9 +124,20 @@ export default class Clock {
         ctx.moveTo(0,0);
         ctx.lineTo(x, y);
         ctx.stroke();
+        // Seconds tail
+        ctx.lineCap = 'butt';
+        r = -(size / 2)*.2;
+        angle = this.s*6+180;
+        x = r*Math.cos((angle+90)*Math.PI/180);
+        y = r*Math.sin((angle+90)*Math.PI/180);
+        ctx.beginPath();
+        ctx.lineWidth = size / 50;
+        ctx.moveTo(0,0);
+        ctx.lineTo(x, y);
+        ctx.stroke();
 
         // Finally, the central dot
-        ctx.fillStyle = 'lightsteelblue';
+        ctx.fillStyle = this.fgColor;
         dot(ctx, 0, 0, size / 40);
     }
 
--- Clock/js/main.js
+++ Clock/js/main.js
@@ -2,10 +2,14 @@ import Clock from './Clock.js';
 
 let clock = new Clock();
 let canvas = clock.toCanvas(innerHeight*0.8);
+// clock.handlersColor = 'orange';
+// clock.bgColor = 'midnightblue';
+// clock.fgColor = 'goldenrod';
+// clock.name = '@jmcareaga';
 document.body.appendChild(canvas);
 
 (function animate() {
     requestAnimationFrame(animate);
-    clock.update();
+    clock.setSystemTime();
     clock.draw(canvas);
 })();
