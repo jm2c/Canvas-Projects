62c549b8fa8b33aba1d05c947bc20f8b22765340
Juan Manuel Careaga <jmcareaga@ciencias.unam.mx>
2019-07-14 18:19:17 -0500

    Nim v1.0

--- Nim/js/Nim.js
+++ Nim/js/Nim.js
@@ -9,13 +9,19 @@
  */
 
 export default class Nim {
-    constructor (initBalls = 20, maxBallsToTake = 3, lastBallLose = true) {
+    constructor (initBalls = 20, maxBallsToTake = 3, lastBallWin = false) {
         this.balls          = initBalls;
         this.maxBallsToTake = maxBallsToTake;
-        this.lastBallLose   = lastBallLose;
+        this.lastBallWin    = lastBallWin;
         this.players = [ new Player(), new Player() ];
         this.players[0].active = true;
         this.finished = false;
+
+        // For canvas properties
+        this.cBalls = []
+        this.rows = Math.floor( Math.sqrt(initBalls) );
+        this.cols = Math.ceil( initBalls / this.rows );
+        this.ratio = this.rows / this.cols;
     }
 
     get active() {
@@ -37,7 +43,7 @@ export default class Nim {
         if (nBalls < 1 || nBalls > this.maxBallsToTake) throw 'Illegal move';
         this.balls -= nBalls;
         this.players[this.active].add(nBalls);
-        
+
         if ( this.ended() ) {
             this.finished = true;
             console.log('Winner is player ' + (this.winner+1));
@@ -48,7 +54,7 @@ export default class Nim {
     ended() {
         if (this.balls > 0) return false;
         this.players.forEach( player => {
-            if (this.lastBallLose) {
+            if (!this.lastBallWin) {
                 if ( !player.active )
                     player.winner = true;
             } else {
@@ -57,6 +63,59 @@ export default class Nim {
             }
         });
         return true;
+    }
+
+    toCanvas(size) {
+        const canvas = document.createElement('canvas');
+        canvas.width = size;
+        canvas.height = size * this.ratio;
+
+        const ballRadius = size / this.cols;
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                if (this.cBalls.length == this.balls) break;
+                const ball = new Ball((j+.5)*ballRadius, (i+0.5)*ballRadius, ballRadius*.8);
+                ball.index = {i:i+1, j:j+1};
+                this.cBalls.push(ball);
+            }
+        }
+
+        this.draw(canvas);
+        return canvas;
+    }
+
+    draw(canvas) {
+        canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height);
+        this.cBalls.forEach( ball => {
+            ball.draw(canvas);
+        });
+    }
+
+    getBall(i, j) {
+        for (let n = 0; n < this.cBalls.length; n++) {
+            if (this.cBalls[n].index.i == i && this.cBalls[n].index.j == j) {
+                return this.cBalls[n];
+            }
+        }
+    }
+
+    removeFromCanvas(i, j, canvas) {
+        for (let n = 0; n < this.cBalls.length; n++) {
+            if (this.cBalls[n].index.i == i && this.cBalls[n].index.j == j) {
+                this.cBalls.splice(n, 1);
+                break;
+            }
+        }
+        this.draw(canvas);
+    }
+
+    removeSelected(canvas) {
+        for (let n = this.cBalls.length-1; n >= 0; n--) {
+            if (this.cBalls[n].selected) {
+                this.cBalls.splice(n, 1);
+            }
+        }
+        this.draw(canvas);
     }
 }
 
@@ -71,5 +130,23 @@ class Player {
     }
     toggleActive() {
         this.active = !this.active;
+    }
+}
+
+class Ball {
+    constructor(x, y, size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.selected = false;
+        this.index = undefined;
+    }
+
+    draw(canvas) {
+        const ctx = canvas.getContext('2d');
+        ctx.beginPath();
+        ctx.fillStyle = this.selected ? 'forestgreen' : 'silver';
+        ctx.arc(this.x, this.y, this.size / 2, 0, 2*Math.PI);
+        ctx.fill();
     }
 }
--- Nim/js/main.js
+++ Nim/js/main.js
@@ -1,12 +1,77 @@
 import Nim from './Nim.js';
 
-const game = new Nim(100, 15, false);
+// Game constants
+const initBalls = 20,
+      maxBallsPerTurn = 3,
+      lastBallWin = false;
 
-while (!game.finished){
-    var balls = Math.floor(Math.random()*15 + 1);
-    console.log(
-        `Player ${game.active + 1} takes ${balls} balls,` +
-        `${game.balls - balls} remaining.`
-    );
-    game.move(balls);
+// Game info
+let taken = 0;
+function updateLabel() {
+    const info = document.getElementById('info');
+    const activePlayer = game.active + 1;
+    if (!game.finished) {
+        info.innerText = `Player ${activePlayer} select ${taken} balls.`;
+    } else {
+        const winner = game.winner + 1;
+        info.innerText = `Player ${winner} wins!`;
+    }
 }
+
+// Draw canvas
+let game = new Nim(initBalls, maxBallsPerTurn, lastBallWin);
+const canvas = game.toCanvas(300);
+const board = document.getElementById('board');
+canvas.style.backgroundColor = '#666';
+board.appendChild(canvas);
+updateLabel();
+
+// Select balls
+const boundries = canvas.getBoundingClientRect();
+canvas.addEventListener('click', evt => {
+    const x = evt.clientX - boundries.x;
+    const y = evt.clientY - boundries.y;
+    const i = Math.ceil(y / (canvas.height / game.rows));
+    const j = Math.ceil(x / (canvas.width / game.cols));
+    const ball = game.getBall(i, j) || {};
+    if (dist(ball.x, ball.y, x, y) < ball.size / 2) {
+        if (ball.selected) {
+            ball.selected = false;
+            taken--;
+        } else {
+            if (taken < maxBallsPerTurn) {
+                ball.selected = true;
+                taken++;
+            }
+        }
+        game.draw(canvas);
+        updateLabel();
+    }
+});
+
+// Change player
+const btn = document.getElementById('pass');
+btn.addEventListener('click', function() {
+    if (taken == 0) return;
+    game.move(taken);
+    game.removeSelected(canvas);
+    taken = 0;
+    updateLabel();
+});
+
+// Calculate the distance between two points
+function dist (x1, y1, x2, y2) {
+    return Math.sqrt( (x2-x1)**2 + (y2-y1)**2 );
+}
+
+/**
+ * Random game in console
+ */
+// while (!game.finished){
+//     var balls = Math.floor(Math.random()*game.maxBallsToTake + 1);
+//     console.log(
+//         `Player ${game.active + 1} takes ${balls} balls,` +
+//         `${game.balls - balls} remaining.`
+//     );
+//     game.move(balls);
+// }
